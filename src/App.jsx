4b5859e06d3a5d91b06cac52cb97cf0a005099fc
mamelilea <achmalpradiptaaditama@gmail.com>
2024-08-03 21:35:46 +0700

    edit: adjustment section

--- src/App.jsx
+++ src/App.jsx
@@ -1,30 +1,30 @@
 import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import Home from './views/Home.jsx';
-import Profil from './views/profil.jsx';
-import Berita from './views/Berita.jsx';
-import Wisata from './views/Wisata.jsx';
-import UMKM from './views/UMKM.jsx';
+import Home from './views/Home/Home.jsx';
+import Profil from './views/Profil/Profil.jsx';
+import Berita from './views/Berita/Berita.jsx';
+import Wisata from './views/Wisata/Wisata.jsx';
+import UMKM from './views/UMKM/UMKM.jsx';
 import NotFound from './views/NotFound.jsx';
 
 
 
 
 const App = () => {
-    return (
-        <>
-            <Router>
-              <Routes>
-                <Route path="/" element={<Home />}/>
-                <Route path="/profil" element={<Profil />}/>
-                <Route path="/berita" element={<Berita />}/>
-                <Route path="/wisata" element={<Wisata/>}/>
-                <Route path="/UMKM" element={<UMKM />}/>
-                <Route path="/*" element={<NotFound />}/>
-              </Routes>
-            </Router>
-        </>
-    );
+  return (
+    <>
+      <Router>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/profil" element={<Profil />} />
+          <Route path="/berita" element={<Berita />} />
+          <Route path="/wisata" element={<Wisata />} />
+          <Route path="/UMKM" element={<UMKM />} />
+          <Route path="/*" element={<NotFound />} />
+        </Routes>
+      </Router>
+    </>
+  );
 };
 
 export default App;
