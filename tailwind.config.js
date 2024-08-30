0bb8d8a3272534f2ad3605cdad140ce39446cd9a
faracutrisfa <faracutrisfa@gmail.com>
2024-08-30 20:42:38 +0700

    feat: gatau lupa

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,21 +1,21 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: [
-    "./index.html",
-    "./src/**/*.{js,ts,jsx,tsx}",
-  ],
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
     extend: {
       fontFamily: {
-        poppins: ["Poppins", "sans-serif"]
+        poppins: ["Poppins", "sans-serif"],
       },
       colors: {
         "cust-darkblue": "#348AC9",
         "cust-blue": "#3E9ADD",
         "cust-softblue": "#ECF5FF",
-        "cust-gray" : "#929292",
+        "cust-gray": "#929292",
+      },
+      spacing: {
+        29: "115px",
       },
     },
   },
   plugins: [],
-}
+};
