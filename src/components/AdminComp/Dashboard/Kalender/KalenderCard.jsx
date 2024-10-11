b32daca8cbc4d0b7a8c58d7e7b7d90a8393b6e21
mamelilea <achmalpradiptaaditama@gmail.com>
2024-10-11 22:00:41 +0700

    `fix:`testing admin for kalender

--- src/components/AdminComp/Dashboard/Kalender/KalenderCard.jsx
+++ src/components/AdminComp/Dashboard/Kalender/KalenderCard.jsx
@@ -2,7 +2,7 @@ const KalenderCard = ({ kalender, children }) => {
     return (
         <div className='border rounded-xl shadow w-full h-80 overflow-hidden relative'>
             <img src={kalender.imageURL} alt={kalender.title} className='w-full h-52 object-cover rounded ' />
-            <div className="w-full xl:h-[175px] bg-gradient-to-b from-[#CBE0F8] via-[#E2EDF9] to-[#F2F8FF] px-4 py-3 flex flex-col justify-between rounded-xl absolute bottom-0 right-0 left-0">
+            <div className="w-full xl:h-[175px] bg-gradient-to-b from-[#CBE0F8] via-[#E2EDF9] to-[#F2F8FF] px-4 py-3 flex flex-col justify-between rounded-xl absolute bottom-0 right-0 left-0"> 
                 <div>
                     <span className='text-[10px] text-gray-400'>
                         {new Date(kalender.date).toLocaleDateString('id-ID', { day: 'numeric', month: 'long', year: 'numeric' })}
