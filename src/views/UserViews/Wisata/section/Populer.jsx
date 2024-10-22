0a001d0433e4d249d32e481632d17143c41c6765
faracutrisfa <faracutrisfa@gmail.com>
2024-10-22 20:37:54 +0700

    style: add style some components

--- src/views/UserViews/Wisata/section/Populer.jsx
+++ src/views/UserViews/Wisata/section/Populer.jsx
@@ -42,29 +42,29 @@ const wisataData = [
 
 const Populer = () => {
     return (
-        <div className="bg-cust-softblue bg-cover bg-[url('images/Landing/WisataUMKMSection/bgPattern.png')] w-full flex-row items-center font-poppins justify-center py-28">
-            <div className='space-y-20 relative w-full container mx-auto'>
-            <div className='flex justify-center items-center w-full'>
-                <img src="https://res.cloudinary.com/ddlo3v9hx/image/upload/v1728371039/Group_238040_lihsi5.png" alt="" className='lg:w-1/3 w-3/4'/>
-            </div>
-            <div className=' w-full min-h-screen flex justify-center relative items-center'>
-                <div className="lg:w-full lg:h-full rounded-lg py-10 overflow-hidden">
-                <div className="w-full h-full flex flex-wrap items-center justify-center gap-12">
-                {wisataData.slice(0, 6).map((card, index) => (
-                         
+        <section className="font-poppins bg-cust-softblue bg-cover bg-[url('images/Landing/WisataUMKMSection/bgPattern.png')]">
+            <div className='flex flex-col lg:pt-52 pt-32 md:pb-36 pb-9 px-5 lg:px-0'>
+                <div className='flex justify-center lg:mb-16 mb-9 md:lg-12'>
+                    <img src="https://res.cloudinary.com/ddlo3v9hx/image/upload/v1728371039/Group_238040_lihsi5.png" alt="" className='lg:w-[473px] lg:h-[97px] w-[234px] h-[48px]' />
+                </div>
+                <div className='w-full flex justify-center relative items'>
+                    <div className='lg:w-full lg:h-full rounded-lg overflow-hidden'>
+                        <div className="w-full flex flex-wrap items-center justify-center md:gap-12 gap-4">
+                            {wisataData.slice(0, 6).map((card, index) => (
+
                                 <WisataUMKMCard
                                     type={card.type}
                                     image={card.image}
                                     title={card.title}
                                     location={card.location}
                                 />
-                        
-                        ))}
+
+                            ))}
+                        </div>
                     </div>
                 </div>
             </div>
-            </div>
-        </div>
+        </section>
     );
 };
 
