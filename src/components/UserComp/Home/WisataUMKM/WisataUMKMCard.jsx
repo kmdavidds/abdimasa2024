5848ed4729338ac398f76fcf4725d0718e9e9324
faracutrisfa <faracutrisfa@gmail.com>
2024-10-24 10:05:57 +0700

    style: add style populer and wisata card

--- src/components/UserComp/Home/WisataUMKM/WisataUMKMCard.jsx
+++ src/components/UserComp/Home/WisataUMKM/WisataUMKMCard.jsx
@@ -21,15 +21,15 @@ const WisataUMKMCard = ({ type, image, title, location, name, priceRange, waLink
 
     if (type === 'wisata') {
         return (
-            <div className="bg-white relative font-poppins  rounded-lg overflow-hidden w-full md:w-1/3 md:max-w-[360px] max-w-[150px] max-h-[175px] md:max-h-[416px]">
+            <div className="bg-white relative font-poppins rounded-lg overflow-hidden w-full md:max-w-[360px] max-w-[150px] max-h-[175px] md:max-h-[416px]">
                 <img
                     src={image}
                     alt={title}
-                    className="w-full h-[175px] md:h-[416px] object-cover"
+                    className="w-full h-[175px] md:h-60 xl:h-[416px] lg:h-72 object-cover"
                 />
-                <div className="p-3 md:p-4 absolute bottom-0 flex flex-col justify-end h-full bg-gradient-to-t from-gray-950/90 via-gray-950/20 to-transparent w-full text-white">
-                    <h3 className="text-xs md:text-2xl font-bold">{title}</h3>
-                    <p className="md:text-base text-[8px] text-opacity-70">{location}</p>
+                <div className="p-3 lg:p-4 absolute bottom-0 flex flex-col justify-end h-full bg-gradient-to-t from-gray-950/90 via-gray-950/20 to-transparent w-full text-white">
+                    <h3 className="text-xs xl:text-2xl lg:text-xl font-bold">{title}</h3>
+                    <p className="lg:text-base text-[8px] text-opacity-70">{location}</p>
                 </div>
             </div>
         )
@@ -41,12 +41,12 @@ const WisataUMKMCard = ({ type, image, title, location, name, priceRange, waLink
                 <img
                     src={image}
                     alt={name}
-                    className="w-full h-[175px] md:h-[416px] object-cover"
+                    className="w-full h-[175px] lg:h-[416px] object-cover"
                 />
-                <div className="p-3 md:p-4 absolute bottom-0 text-white flex justify-between gap-1.5 sm:gap-0 items-end w-full h-full bg-gradient-to-t from-gray-950/90 via-gray-950/20 to-transparent">
+                <div className="p-3 lg:p-4 absolute bottom-0 text-white flex justify-between gap-1.5 sm:gap-0 items-end w-full h-full bg-gradient-to-t from-gray-950/90 via-gray-950/20 to-transparent">
                     <div>
-                        <h3 className="text-xs md:text-2xl font-bold">{name}</h3>
-                        <p className="md:text-base text-[8px] text-opacity-70">{priceRange}</p>
+                        <h3 className="text-xs lg:text-2xl font-bold">{name}</h3>
+                        <p className="lg:text-base text-[8px] text-opacity-70">{priceRange}</p>
                     </div>
                     <a href={""} onClick={waClick} target="_blank" rel="noopener noreferrer" className="text-green-500 bg-white p-2 rounded-full">
                         <IoLogoWhatsapp size={isMobile ? 16 : 36} />
