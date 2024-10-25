5e152502f3ee35928f2dd8e2bd7eee26537d4bc0
mamelilea <achmalpradiptaaditama@gmail.com>
2024-10-25 22:01:32 +0700

    style: add responsive admin login

--- src/views/AdminViews/Login/LoginAdmin.jsx
+++ src/views/AdminViews/Login/LoginAdmin.jsx
@@ -9,24 +9,23 @@ const LoginAdmin = () => {
         return <Navigate to="/admin" />;
     }
     return (
-        <div className="bg-cust-softblue bg-[url('/images/Admin/bgLogin.svg')] bg-cover  w-full flex items-center justify-center">
-            <div className='container max-w-[80%] h-screen flex items-center justify-center'>
-                <div className='w-1/2 flex flex-col items-center justify-center gap-14'>
-                    <img src="/images/Admin/titleLogin.svg" alt="Halo Admin" />
-                    <img src="/images/Admin/Maskot.svg" alt="Maskot" />
-                </div>
-                <div className='w-1/2 flex flex-col items-center justify-center gap-10 '>
-                    {/* title */}
-                    <div className=' text-center space-y-6'>
-                        <h2 className='font-bold text-4xl'>Log In</h2>
-                        <p className='text-2xl text-gray-500'>Silahkan login terlebih dahulu</p>
+        <section className="font-poppins bg-cust-softblue bg-[url('/images/Admin/bgLogin.svg')]">
+            <div className='lg:px-32 px-10 pt-20'>
+                <div className='flex flex-col lg:flex-row items-center gap-14 lg:gap-20 xl:gap-56 justify-center'>
+                    <div className='flex flex-col items-center gap-10'>
+                        <img src="/images/Admin/titleLogin.svg" alt="Halo Admin" className='lg:w-[430px] lg:h-[97px] w-[243px] h-[55px]'/>
+                        <img src="/images/Admin/Maskot.svg" alt="Maskot" className='w-2/3 lg:w-full'/>
                     </div>
-                    {/* form */}
-                    <FormLogin />
-
+                    <div>
+                        <div className='flex flex-col gap-6 items-center'>
+                            <h1 className='font-bold text-4xl'>Log In</h1>
+                            <p className='xl:text-2xl text-xl text-gray-500'>Silahkan login terlebih dahulu</p>
+                        </div>
+                        <FormLogin />
+                    </div>
                 </div>
             </div>
-        </div>
+        </section>
     )
 }
 
